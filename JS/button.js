3e7d8ede2bd6534927414e7d4b664ff5bdd217b5
ickx <mizoudaaa@gmail.com>
2022-11-03 20:52:34 +0100

    sidebar & background padding fix

--- JS/button.js
+++ JS/button.js
@@ -1,16 +1,19 @@
 let compteur = false;
 
+let padLeftBase = "15%";
+let padAdded = "200px";
+
 document.querySelector(":root").style.setProperty("--additional-padding", 200);
 var padd = $(".corps").css("padding-left");
 
 $("#logoTroisBarres").click(function () {
     if (!compteur) {
         $(".bouttonsColonne").css("display", "block");
-        $(".corps").css("padding-left","additional-padding");
+        $(".corps").css("padding-left", `calc(${padLeftBase} + ${padAdded})`);
     }
     else {
         $(".bouttonsColonne").css("display", "none");
-        $(".corps").css("padding-left", padd);
+        $(".corps").css("padding-left", `calc(${padLeftBase})`);
     }
     compteur = !compteur;
 });
