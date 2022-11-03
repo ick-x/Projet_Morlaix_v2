03015bcc0bb3f554c55813c65b063255c139b97a
ickx <mizoudaaa@gmail.com>
2022-11-03 17:26:05 +0100

    background image & mise en page

--- JS/button.js
+++ JS/button.js
@@ -1,12 +1,16 @@
 let compteur = false;
+
+document.querySelector(":root").style.setProperty("--additional-padding", 200);
+var padd = $(".corps").css("padding-left");
+
 $("#logoTroisBarres").click(function () {
     if (!compteur) {
-        $("#bouttonsColonne").css("display", "block");
-        $(".corps").css("margin-left", "200px");
+        $(".bouttonsColonne").css("display", "block");
+        $(".corps").css("padding-left","additional-padding");
     }
     else {
-        $("#bouttonsColonne").css("display", "none");
-        $(".corps").css("margin-left", "0");
+        $(".bouttonsColonne").css("display", "none");
+        $(".corps").css("padding-left", padd);
     }
     compteur = !compteur;
 });
