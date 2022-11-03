f1792b4eaf1755ea8c67c6d93ee57e3252c30395
ickx <mizoudaaa@gmail.com>
2022-11-03 21:58:15 +0100

    Sidebar & Navbar fix

--- JS/button.js
+++ JS/button.js
@@ -8,12 +8,20 @@ var padd = $(".corps").css("padding-left");
 
 $("#logoTroisBarres").click(function () {
     if (!compteur) {
-        $(".bouttonsColonne").css("display", "block");
-        $(".corps").css("padding-left", `calc(${padLeftBase} + ${padAdded})`);
+        if (!window.matchMedia('(max-width: 1096px)').matches) {
+            $(".bouttonsColonne").css("display", "block");
+            $(".corps").css("padding-left", `calc(${padLeftBase} + ${padAdded})`);
+        }
+        else {
+            $(".bouttonsColonne").css("display", "block");
+        }
     }
     else {
         $(".bouttonsColonne").css("display", "none");
-        $(".corps").css("padding-left", `calc(${padLeftBase})`);
+        if (!window.matchMedia('(max-width: 1096px)').matches)
+            $(".corps").css("padding-left", `calc(${padLeftBase})`);
+
+
     }
     compteur = !compteur;
 });
